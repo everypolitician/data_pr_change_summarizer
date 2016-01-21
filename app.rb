54471a726e4a9c1aa2a95e0870869e886c521aa7
Chris Mytton <chrismytton@gmail.com>
2016-01-21 16:56:49 +0000

    Gather stats into a hash

--- app.rb
+++ app.rb
@@ -9,19 +9,23 @@ class PullRequestReview
     pull_request = github.pull_request(repository_full_name, number)
     files = github.pull_request_files(repository_full_name, number)
     ep_popolo = files.find_all { |file| file[:filename].match(/ep-popolo-v1.0\.json$/) }
+    stats = {}
     ep_popolo.each do |file|
       # Get the JSON and parse it
       before = JSON.parse(open(file[:raw_url].sub(pull_request[:head][:sha], pull_request[:base][:sha])).read)
       after = JSON.parse(open(file[:raw_url]).read)
-      stats = {}
       %w[persons organizations].each do |collection|
-        before_map = Hash[before[collection].map { |item| [item['id'], item] }]
-        after_map = Hash[after[collection].map { |item| [item['id'], item] }]
+        stats[file] ||= {}
+        stats[file][collection] ||= {}
         before_ids = before[collection].map { |item| item['id'] }
         after_ids = after[collection].map { |item| item['id'] }
         logger.info "collection=#{collection} added=#{(after_ids - before_ids).size} removed=#{(before_ids - after_ids).size}"
+
+        stats[file][collection]['added'] = after_ids - before_ids
+        stats[file][collection]['removed'] = before_ids - after_ids
       end
     end
+    p stats
   end
 
   def handle_webhook
