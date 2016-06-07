0ddd9205a2e6c50396d9cb311ff7d6b66f64ba38
Tony Bowden <tony@tmtm.com>
2016-06-07 20:07:26 +0100

    Add test for changing Wikidata identifiers
    Report when someone changes (or loses) their Wikidata identifier

--- app.rb
+++ app.rb
@@ -50,6 +50,19 @@ class ComparePopolo
     before.persons - after.persons
   end
 
+  def wikidata_links_changed
+    prev = Hash[before.persons.map { |p| [p.id, p.wikidata] }]
+    post = Hash[after.persons.map  { |p| [p.id, p.wikidata] }]
+    in_both = prev.keys & post.keys
+    in_both.select { |id| prev[id] != post[id] }.map do |id|
+      { 
+        id: id,
+        was: prev[id] || 'none',
+        now: post[id] || 'none',
+      }
+    end
+  end
+
   def organizations_added
     after.organizations - before.organizations
   end
