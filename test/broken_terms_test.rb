50998eff0bdcba2257010753a3ed56d6999c3dbc
Oliver Denman <oliver@therewillbeanimals.com>
2016-07-27 14:26:50 +0100

    update test to check erb

--- test/broken_terms_test.rb
+++ test/broken_terms_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-describe 'BrokenTerms' do
+describe 'ComparePopolo' do
 
   subject do
     ComparePopolo.parse(
@@ -14,7 +14,42 @@ describe 'BrokenTerms' do
     subject.broken_terms
   end
 
-  it 'should 1' do
+  it 'should a single broken term' do
   	subject.broken_terms.size.must_equal 1
+  end
+end
+
+describe ReviewChanges do
+  let(:before_after) do
+    [
+      {
+        before: {
+          events: [
+          	{ classification: 'legislative period', id: 'term/23' }
+          ]
+        }.to_json,
+        after: {
+        	events: [
+          	{ classification: 'legislative period', id: 'term/23' },
+          	{ classification: 'legislative period', id: 'term/dog' },
+          	{ classification: 'legislative period', id: 'term/bag' }
+          ]
+        }.to_json,
+        path: 'foo/bar.json'
+      }
+    ]
+  end
+
+  subject { ReviewChanges.new(before_after) }
+
+  it 'should report any broken terms in the comments' do
+    comment = subject.to_html
+    assert comment.include?('broken:')
+  end
+
+  it 'should report any broken terms in the comments' do
+    comment = subject.to_html
+    assert comment.include?('dog')
+    assert comment.include?('bag')
   end
 end
