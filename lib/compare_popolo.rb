8707a5e2e1966842f25be0d848df1041c8a9ea11
Tony Bowden <tony@tmtm.com>
2016-08-03 17:38:40 +0100

    extract PeopleAdditionalNames report

--- lib/compare_popolo.rb
+++ lib/compare_popolo.rb
@@ -15,34 +15,12 @@ class ComparePopolo
     @path = options[:path]
   end
 
-  def before_names
-    @name_hash_pre ||= Hash[before.persons.map { |p| [p.id, p.name] }]
-  end
-
-  def after_names
-    @name_hash_post ||= Hash[after.persons.map { |p| [p.id, p.name] }]
-  end
-
   def people_name_changes
     Report::PeopleNames.new(before, after).changed
   end
 
   def people_additional_name_changes
-    all_names = lambda do |p|
-      other_names = p.other_names.map { |n| n[:name] } rescue []
-      (other_names | [p.name]).to_set
-    end
-    names_all_pre =  Hash[before.persons.map { |p| [p.id, all_names.call(p)] }]
-    names_all_post = Hash[after.persons.map  { |p| [p.id, all_names.call(p)] }]
-    in_both = names_all_pre.keys & names_all_post.keys
-    in_both.select { |id| names_all_pre[id] != names_all_post[id] }.map do |id|
-      {
-        id:      id,
-        name:    before_names[id],
-        removed: (names_all_pre[id] - names_all_post[id]).to_a,
-        added:   (names_all_post[id] - names_all_pre[id]).to_a,
-      }
-    end
+    Report::PeopleAdditionalNames.new(before, after).changed
   end
 
   def people_added
@@ -103,6 +81,26 @@ class Report
           id:  id,
           was: before_names[id],
           now: after_names[id],
+        }
+      end
+    end
+  end
+
+  class PeopleAdditionalNames < PeopleNames
+    def changed
+      all_names = lambda do |p|
+        other_names = p.other_names.map { |n| n[:name] } rescue []
+        (other_names | [p.name]).to_set
+      end
+      names_all_pre =  Hash[before.persons.map { |p| [p.id, all_names.call(p)] }]
+      names_all_post = Hash[after.persons.map  { |p| [p.id, all_names.call(p)] }]
+      in_both = names_all_pre.keys & names_all_post.keys
+      in_both.select { |id| names_all_pre[id] != names_all_post[id] }.map do |id|
+        {
+          id:      id,
+          name:    before_names[id],
+          removed: (names_all_pre[id] - names_all_post[id]).to_a,
+          added:   (names_all_post[id] - names_all_pre[id]).to_a,
         }
       end
     end
