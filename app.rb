3adf07379c1123ad8980bddc9b874d17a750da2a
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-08-03 14:24:51 +0100

    Move ReviewChanges class to a separate file

--- app.rb
+++ app.rb
@@ -5,24 +5,7 @@ require 'open-uri'
 require 'erb'
 require 'find_popolo_files'
 require 'compare_popolo'
-
-class ReviewChanges
-  attr_reader :popolo_files
-
-  def initialize(popolo_before_after)
-    @popolo_files = popolo_before_after.map do |opts|
-      ComparePopolo.parse(opts)
-    end
-  end
-
-  def to_html
-    template.result(binding)
-  end
-
-  def template
-    @template ||= ERB.new(File.read('comment_template.md.erb'))
-  end
-end
+require 'review_changes'
 
 class PullRequestReview
   include WebhookHandler
