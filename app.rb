978c77bcf1eb4962e003d38d0b54bbe4fcb267a2
Oliver Denman <oliver@therewillbeanimals.com>
2016-08-03 13:17:30 +0100

    Move FindPopoloFiles to separate file

--- app.rb
+++ app.rb
@@ -6,12 +6,5 @@ require 'erb'
 require 'compare_popolo'
 require 'review_changes'
 require 'pull_request_review'
-
-class FindPopoloFiles
-  POPOLO_FILE_REGEX = /ep-popolo-v(\d+\.)?(\d+\.)?\d+\.json$/
-
-  def self.from(files)
-    files.select { |file| file[:filename].match(POPOLO_FILE_REGEX) }
-  end
-end
+require 'find_popolo_files'
 
