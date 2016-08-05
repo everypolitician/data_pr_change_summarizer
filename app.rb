f81f60c1dbccc318027484995b4569422fda6771
Tony Bowden <tony@tmtm.com>
2016-08-05 14:39:10 +0100

    auto-require everything in lib/
    By default 'lib' isn't in the LOAD_PATH, so these 'require's are
    failing. Rather than twiddling that, or using 'require_relative', we
    can use 'require_all', as we're already using that anyway.

--- app.rb
+++ app.rb
@@ -3,7 +3,5 @@ Bundler.require
 
 require 'open-uri'
 require 'erb'
-require 'find_popolo_files'
-require 'compare_popolo'
-require 'review_changes'
-require 'pull_request_review'
+
+require_all 'lib'
