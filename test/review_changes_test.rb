ec65362fed51a91ea1f1a151ec2676389c257c64
Oliver Denman <oliver@therewillbeanimals.com>
2016-08-09 09:30:11 +0100

    Test that erb template renders added and removed terms

--- test/review_changes_test.rb
+++ test/review_changes_test.rb
@@ -13,6 +13,18 @@ describe ReviewChanges do
             { id: 'abc', name: 'Reds' },
             { id: 'def', name: 'Greens' },
           ],
+          events:        [
+            {
+              classification: 'legislative period',
+              id:             'term/52',
+              name:           '52nd Parliament of the United Kingdom',
+            },
+            {
+              classification: 'legislative period',
+              id:             'term/53',
+              name:           '53rd Parliament of the United Kingdom',
+            },
+          ],
         }.to_json,
         after:  {
           persons:       [
@@ -22,6 +34,18 @@ describe ReviewChanges do
           organizations: [
             { id: 'abc', name: 'Reds' },
             { id: 'ghi', name: 'Blues' },
+          ],
+          events:        [
+            {
+              classification: 'legislative period',
+              id:             'term/53',
+              name:           '53rd Parliament of the United Kingdom',
+            },
+            {
+              classification: 'legislative period',
+              id:             'term/54',
+              name:           '54th Parliament of the United Kingdom',
+            },
           ],
         }.to_json,
         path:   'foo/bar.json',
@@ -38,5 +62,7 @@ describe ReviewChanges do
     assert comment.include?('- `456` - Alice')
     assert comment.include?('- `def` - Greens')
     assert comment.include?('- `ghi` - Blues')
+    assert comment.include?('- `term/54` - 54th Parliament of the United Kingdom')
+    assert comment.include?('- `term/52` - 52nd Parliament of the United Kingdom')
   end
 end
